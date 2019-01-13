20e299b1fb44898c25c422e1a7a5ff3facea0b77
rafaelchiafarelli <rafael.chiafarelli@gmail.com>
2019-01-13 09:25:42 -0200

    first commit

--- EstudioPoliteama/settings.py
+++ EstudioPoliteama/settings.py
@@ -98,8 +98,8 @@ USE_TZ = True
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'programacaoparamakers@gmail.com'
-EMAIL_HOST_PASSWORD = 'ProgramaMakers1234'
+EMAIL_HOST_USER = 'your@gmail.com'
+EMAIL_HOST_PASSWORD = 'yourpassword'
 EMAIL_PORT = 587
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
